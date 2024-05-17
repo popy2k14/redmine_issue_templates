0a63f699af582c6f44ecc502a00009d0cd402ab8
popy2k14 <tobias.diendorfer@gmail.com>
2024-05-17 12:07:10 +0200

    Update 2014020191500_add_should_replaced_to_issue_template_settings.rb
    Change should_replaced default value from false to true

--- db/migrate/2014020191500_add_should_replaced_to_issue_template_settings.rb
+++ db/migrate/2014020191500_add_should_replaced_to_issue_template_settings.rb
@@ -1,6 +1,6 @@
 class AddShouldReplacedToIssueTemplateSettings < ActiveRecord::Migration[4.2]
   def self.up
-    add_column :issue_template_settings, :should_replaced, :boolean, default: false
+    add_column :issue_template_settings, :should_replaced, :boolean, default: true
   end
 
   def self.down
